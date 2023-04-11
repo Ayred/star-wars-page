f407af725440b57de045f39a9c6c3dcdd0b0e738
Ayred Antara Caicedo Torres <ayredantara@gmail.com>
2023-04-11 10:51:21 -0700

    static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,43 +1,48 @@
-import colors from 'vuetify/es5/util/colors';
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  target: "static",
+  router: {
+    base: "/star-wars-page/",
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Star Wars Movies',
+    title: "Star Wars Movies",
     htmlAttrs: {
-      lang: 'en',
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   env: {
-    imageApiKey: process.env.IMAGE_API_KEY || '9106b62a',
+    imageApiKey: process.env.IMAGE_API_KEY || "9106b62a",
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/helpers'],
+  plugins: ["~/plugins/helpers"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['@nuxtjs/vuetify'],
+  buildModules: ["@nuxtjs/vuetify"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/axios'],
+  modules: ["@nuxtjs/axios"],
 
   axios: {
     // proxy: true
-    baseURL: 'https://swapi.dev/api/',
+    baseURL: "https://swapi.dev/api/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
